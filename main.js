4c70eb32c8f3c6159896f7e29f29c4bb1437cede
Orbter <assouline39@gmail.com>
2024-03-02 14:03:57 +0200

    made the basic html for this project

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
+const x = 15;
+
+console.log(x);
 
 /******/ })()
 ;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL3RlbXBsYXRlLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnN0IHggPSAxNTtcclxuXHJcbmNvbnNvbGUubG9nKHgpO1xyXG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
